69b3ce3433cfccfd4e68cbfb00bf4e92185266a6
carcasonne <villumsonne@tutanota.com>
2023-08-30 15:28:25 +0200

    Begin stable

---
+++ Week1_ProblemA_StablePerfectMatching/Week1_ProblemA_StablePerfectMatching/Domain/Vertex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Week1_ProblemA_StablePerfectMatching.Domain;
+
+public class Vertex
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+	public bool IsProposer { get; set; }
+	public IEnumerable<string> Preferences { get; set; }
+	
+	public Vertex(int id, string name, IEnumerable<string> preferences, bool isProposer)
+	{
+		Id = id;
+		Name = name;
+		IsProposer = isProposer;
+		Preferences = preferences;
+	}
+}
+
---
+++ Week1_ProblemA_StablePerfectMatching/Week1_ProblemA_StablePerfectMatching/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Week1_ProblemA_StablePerfectMatching.Domain;
+
+var n = Console.ReadKey().KeyChar;
+Console.ReadKey(); // Discard space
+var m = Console.ReadKey().KeyChar;
+
+var vertices = new List<Vertex>();
+var halfN = n / 2; // cache
+
+// Read n lines of vertices + its preferences
+for(int i = 0; i < n; i++)
+{
+    var line = Console.ReadLine() ?? throw new ArgumentOutOfRangeException("No line could be read");
+    var names = line.Split(" ");
+    var vertexName = names[0];
+    var preferences = names.Skip(1).ToList();
+    var isProposer = i + 1 < halfN;
+
+    var vertex = new Vertex(i, vertexName, preferences, isProposer);
+    vertices.Add(vertex);
+}
+
