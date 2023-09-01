098f07f69eb5ab6f656ddf54584701c2fc84e701
carcasonne <villumsonne@protonmail.com>
2023-09-01 18:52:36 +0200

    StableMatching works now but slow as fuck

--- Week1_ProblemA_StablePerfectMatching/Week1_ProblemA_StablePerfectMatching/Domain/Vertex.cs
+++ Week1_ProblemA_StablePerfectMatching/Week1_ProblemA_StablePerfectMatching/Domain/Vertex.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 namespace Week1_ProblemA_StablePerfectMatching.Domain;
 
 public class Vertex
--- Week1_ProblemA_StablePerfectMatching/Week1_ProblemA_StablePerfectMatching/Program.cs
+++ Week1_ProblemA_StablePerfectMatching/Week1_ProblemA_StablePerfectMatching/Program.cs
@@ -33,48 +33,32 @@ for(var i = 0; i < n; i++)
 
     var vertex = new Vertex(vertexName);
     Vertices.Add(vertex);
+    vertex.RejectorPreferences = preferences;
 
     foreach (var preference in preferences)
     {
         mCount++;
-
         var edge = new Edge(vertexName, preference);
-
-        if (!Edges.Contains(edge))
-        {
-            if (!vertex.IsProposer)
-            {
-                vertex.IsProposer = true;
-                var edges = preferences.Select(x => new Edge(vertexName, x)).Reverse().ToList();
-                vertex.ProposerPreferences = new Stack<Edge>(edges);
-            }
-        }
-        else
-        {
-            if (!vertex.IsRejector)
-            {
-                vertex.IsRejector = true;
-                vertex.RejectorPreferences = preferences;
-            }
-        }
-
         Edges.Add(edge);
     }
 }
 
-var Proposers = Vertices.Where(x => x.IsProposer).ToList();
-var Rejectors = Vertices.Where(x => x.IsRejector).ToList();
+//var Proposers = Vertices.Where(x => x.IsProposer).ToList();
+//var Rejectors = Vertices.Where(x => x.IsRejector).ToList();
 
-while (true)
+var runs = 0;
+
+while(true)
 {
-    var proposer = Proposers.FirstOrDefault(x => !x.IsMatched && !x.IsExhausted);
+    runs++;
+    var proposer = Vertices.FirstOrDefault(v => !v.IsMatched && Edges.Any(x => x.FromName == v.Name));
     if (proposer == null)
         break;
 
-    var rejectorEdge = proposer.ProposerPreferences.Pop();
-    var rejector = Rejectors.FirstOrDefault(x => x.Name == rejectorEdge.ToName);
-    if (rejector == null)
-        continue;
+    var edge = Edges.First(x => x.FromName == proposer.Name);
+    Edges.Remove(edge);
+    var rejector = Vertices.Single(v => v.Name == edge.ToName);
+
 
     // If rejector has no matches, she accepts
     if (!rejector.IsMatched)
@@ -89,15 +73,17 @@ while (true)
     // Otherwise she kindly turns down the proposer
 }
 
-if (Proposers.Any(p => !p.IsMatched))
+if (Vertices.Any(v => !v.IsMatched))
 {
     Console.WriteLine("-");
 }
 else
 {
-    var edges = Proposers.Select(x => new Edge(x.Name, x.Match!.Name)).ToHashSet();
+    var edges = Vertices.Select(v => new Edge(v.Name, v.Match!.Name)).ToHashSet();
     foreach (var proposer in edges)
     {
         Console.WriteLine($"{proposer.FromName} {proposer.ToName}");
     }
 }
+
+Console.WriteLine($"Runs in while loop: {runs}");
